c1f3201db296a8df3fc9b5c43c14ec7753f1c54f
SmritiGuragain <smritiguragain111@gmail.com>
2023-03-27 12:10:02 +0545

    url changed

--- subject/views.py
+++ subject/views.py
@@ -5,3 +5,7 @@ from .models import item
 def home(request):
     model=item.objects.all()
     return render(request,'subject.html',{'model':model})
+
+def see(request,value):
+    model=item.objects.all().filter(Semester=value)
+    return render(request,'subject.html',{'model':model})
